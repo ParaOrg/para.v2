058264a39fe8a27f0ec72a3d1cd931e275eb16e0
devyaegis <cascolangabriel@gmail.com>
2026-08-31 03:06:43 +0800

    Fix CORS duplicate header, remove auto GPS zoom, multi-modal verified

--- lambda-route-search/lambda_function.py
+++ lambda-route-search/lambda_function.py
@@ -1314,7 +1314,7 @@ def lambda_handler(event, context):
         return {
             'statusCode': 200,
             'headers': {
-                'Access-Control-Allow-Origin': '*',
+                
                 'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token',
                 'Access-Control-Allow-Methods': 'POST,OPTIONS',
             },
